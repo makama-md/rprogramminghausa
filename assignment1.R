d191cf658cb0454662ae936df4f8e56ac6f74a7d
makama-md <dmakama@aimsammi.org>
2021-06-27 05:55:31 +0100

    added the first assignment.

---
+++ assignment1.R
@@ -0,0 +1,40 @@
+
+NoInsur = c(15.1, 16.3, 24.3, 21.6, 21.2, 15.6, 11.8, 13.7, 
+            18.7, 17.6, 17.3, 12.9, 12.1, 11, 11.1, 14.8, 19.8, 
+            13.2, 13.8, 11.8, 11.2, 9.6, 19.6, 12.1, 17.6, 10.4, 
+            18.1, 10.9, 16.5, 22, 17.2, 15.5, 13.1, 11.1, 17.7, 
+            14.3, 10, 10, 16.4, 11.9, 13.9, 24.4, 13.1, 10.1, 
+            13.1, 12.4, 16.5, 9.4, 15.3)
+
+state = c("Alabama", "Alaska", "Arizona", "Arkansas", 
+          "California", "Colorado", "Connecticut", "Delaware", 
+          "Florida", "Georgia", "Idaho", "Illinois", "Indiana", 
+          "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", 
+          "Maryland", "Massachusetts", "Michigan", "Minnesota", 
+          "Mississippi", "Missouri", "Montana", "Nebraska", 
+          "Nevada", "New Hampshire", "New Jersey", "New Mexico", 
+          "New York", "North Carolina", "North Dakota", "Ohio", 
+          "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", 
+          "South Carolina", "South Dakota", "Tennessee", "Texas", 
+          "Utah", "Vermont", "Virginia", "Washington", 
+          "West Virginia", "Wisconsin", "Wyoming")
+
+CentLong = c(-86.68, 21.5, -111.93, -92.12, -119.27, -105.56, 
+             -72.75, -75.40, -83.81, -83.18, -114.13, -89.50, -86.43, 
+             -93.37, -98.54, -85.77, -91.43, -69.01, -77.25, -71.72, 
+             -86.43, -93.38, -89.88, -92.44, -110.04, -99.68, -117.03, 
+             -71.58, -74.71, -106.03, -75.82, -79.88, -100.30, -82.67, 
+             -98.72, -120.52, -77.60, -71.52, -80.94, -100.24, -85.98, 
+             -99.58, -111.53, -72.53, -79.47, -120.84, -80.19, -89.83, 
+             -107.55)
+CentLat = c(32.63, 61.38, 34.17, 34.75, 37.27, 39.00, 
+            41.53, 39.15, 27.75, 32.68, 45.50, 39.75, 39.81, 41.93, 
+            38.50, 37.88, 30.97, 45.22, 38.81, 42.04, 44.99, 46.44, 
+            32.50, 38.31, 46.68, 41.50, 38.50, 44.03, 40.14, 34.17, 
+            42.76, 35.23, 47.47, 40.20, 35.31, 44.13, 40.99, 41.58, 
+            33.61, 44.21, 35.83, 31.17, 39.50, 43.86, 38.00, 47.27, 
+            38.92, 44.81, 43.00)
+
+#Ex1. Make a data.frame with NoInsur, state, CentLong, CentLat as the columns.
+#Ex2. What is the number of people with insurance(NoInsur) in Alabama, Georgia, Oregon, Nebraska, Wyoming respectively?
+#Ex3. What is the CentLat of Wyoming?
